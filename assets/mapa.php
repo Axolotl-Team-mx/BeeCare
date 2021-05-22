f45de18f537c396d9218090df309050c131e9591
Alvaro Gabriel Gonzalez Martinez <aggm123@icloud.com>
2021-05-22 14:08:03 -0700

    Cambios de back-end.

---
+++ assets/mapa.php
@@ -0,0 +1,18 @@
+<article>
+    <div style="visibility:hidden">
+        Search Type:
+        <input type="radio" value="address" name="searchTypeGroup" checked="checked" />
+    </div>
+    País:
+    <select id="countrySelector">
+        <option value="MX">México</option>
+    </select>
+    <br /><br />
+
+    <div class="ui-widget">
+        <label for="queryTbx">Buscar Lugar: </label>
+        <input id="queryTbx">
+    </div><br />
+
+    <div id="myMap" style="position:relative;width:100%;min-width:290px;height:500px; left:50px;"></div>
+</article>
