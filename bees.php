ba4011ce50c4909db596b6896f471a50d45fc778
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2021-05-22 18:44:36 -0700

    Nueva info al apartado "bees.php"

--- bees.php
+++ bees.php
@@ -11,41 +11,223 @@ define('TITLE', '¿Abejas?');
 
 <body>
     <?php include 'header.php' ?>
-    <header class="masthead" style="background-image:url('assets/img/home-bg.jpg');">
+    <header class="masthead" style="background-image:url('assets/img/portadaFamiliasabejas.gif');">
         <div class="overlay"></div>
         <div class="container">
             <div class="row">
                 <div class="col-md-10 col-lg-8 mx-auto position-relative">
                     <div class="site-heading">
-                        <h1>¿Qué hay de nuevo?</h1><span class="subheading">Aquí encontraras cada una de nuestras nuevas investigaciones o trabajos.</span>
+                        <h1>Familias de abejas</h1><span class="subheading">Aquí encontraras información sobre cada familia de abejas pertenecientes a la superfamilia "Apoidea".</span>
                     </div>
                 </div>
             </div>
         </div>
-    </header>
+    </header>    
     <div class="container">
-        <section class="article-list">
-            <div class="container">
-                <div class="intro">
-                    <h2 class="text-center">Bee Care</h2>
-                    <p class="text-center">Cambiemos la perspectiva en que se visualizan las abejas en México</p>
-                </div>
-                <div class="row articles">
-                    <div class="col-sm-6 col-md-4 item"><a href="#"><img class="img-fluid" src="assets/img/desk.jpg"></a>
-                        <h3 class="name">Article Title</h3>
-                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.</p><a class="action" href="#"><i class="fa fa-arrow-circle-right"></i></a>
-                    </div>
-                    <div class="col-sm-6 col-md-4 item"><a href="#"><img class="img-fluid" src="assets/img/building.jpg"></a>
-                        <h3 class="name">Article Title</h3>
-                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.</p><a class="action" href="#"><i class="fa fa-arrow-circle-right"></i></a>
-                    </div>
-                    <div class="col-sm-6 col-md-4 item"><a href="#"><img class="img-fluid" src="assets/img/loft.jpg"></a>
-                        <h3 class="name">Article Title</h3>
-                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.</p><a class="action" href="#"><i class="fa fa-arrow-circle-right"></i></a>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <div class="row">
+            <h1 class="text-center">Familias de abejas</h1>
+            <p class="text-center">Las abejas más conocidas son las abejas melíferas, responsables de producir la miel que conocemos. Sin embargo, existen cerca de 4.000 tipos de abejas distribuidas en todo el mundo.</p>
+        </div>
+        
+        <div class="text">
+            <h2>¿Cuántos tipos de abejas hay?</h2>
+
+            <img class="col-sm-6 col-md-4 item"><img class="img-fluid" src="assets/img/beefamiliesimage.jpg" width="350px" height="300px"></img>
+
+            <p>Las abejas son insectos pertenecientes a la superfamilia Apoidea. Se alimentan del néctar de las flores y la mayoría de las especies tienen una organización eurosocial: las colmenas están conformadas por una abeja reina, cientos de abejas obreras y algunos zánganos machos. </p>
+            <h2>Clasificación taxonómica de las abejas</h2>
+            <h3>Superfamilia: Apoidea</h3>
+            <h4>Familias</h4>
+                <li>Andrenidae</li>
+                <li>Apidae</li>
+                <li>Colletidae</li>
+                <li>Halictidae</li>
+                <li>Megachilidae</li>
+                <li>Melittidae</li>
+                <li>Stenotritidae</li>
+
+            <h2 class="text-center">Familia andrenidae</h2>
+            <img class="col-sm-6 col-md-4 item"><img class="img-fluid" src="assets/img/andrenidaefamily.jpg" width="370px" height="300px"></img>
+            <h3>Subfamilias</h3>
+            <p>La familia <strong>Andrenidae</strong> contiene 4 subfamilias:</p>
+                <li>Alocandreninae</li>
+                <li>Andreninae</li>
+                <li>Panurginae</li>
+                <li>Oxaeinae</li>
+            
+            <p></p>
+            <h3>Características</h3>
+            <p>Las características principales de este tipo de abejas son:</p>
+                <li>Tienen hábitos solitarios.</li>
+                <li>Es posible encontrarlas en todo el mundo, exceptuando Australia.</li>
+                <li>Cuentan con dos subantenas circundando las antenas principales.</li>
+                <li>Por lo general, construyen sus paneles después de realizar agujeros en el suelo.</li>
+                <li>Se alimentan del néctar de flores específicas.</li>
+
+            <p></p>
+            <h3>Especies</h3>
+            <p>Algunos de los tipos de abejas más representativos de esta familia son:</p>
+                <li>Mesoxaea tachytiformis</li>
+                <li>Alloxaea brevipalpis</li>
+                <li>Protoxaea australis</li>
+                <li>Notoxaea ferrugínea</li>
+                <li>Oxaea schwarzi</li>
+
+            <h2 class="text-center">Familia apidae</h2>
+            <img class="col-sm-6 col-md-4 item"><img class="img-fluid" src="assets/img/apidaefamily.jpg" width="370px" height="300px"></img>
+            <h3>Subfamilias</h3>
+            <p>La familia <strong>Andrenidae</strong> contiene 4 subfamilias:</p>
+                <li>Nomadinae</li>
+                <li>Xylocopine</li>
+                <li>Apinae</li>
+            
+            <p></p>
+            <h3>Características</h3>
+            <p>Está familia incluye gran variedad de especies con características diversas. Estas formas de vida diferentes generan dinámicas variadas en los panales:</p>
+                <li>Algunas forman eurosociedades.</li>
+                <li>Otras tienen hábitos solitarios</li>
+                <li>Algunas abejas solo parasitan los nidos de otras especies.</li>
+                <li>En algunas colonias conviven más de dos hembras que cumplen las mismas funciones.</li>
+                <li>Anidan en el suelo o el tronco de los árboles.</li>
+                <li>En las ciudades es común ver que construyen panales sobre las paredes o en postes eléctricos.</li>
+
+            <p></p>
+            <h3>Especies</h3>
+            <p>Algunas especies de esta familia son:</p>
+                <li>Thyreus albomaculatus</li>
+                <li>Brachynomada cearensis</li>
+                <li>Brachynomada chacoensis</li>
+                <li>Ceratina acantha</li>
+                <li>Ceratina allodapoides</li>
+            
+            <h2 class="text-center">Familia colletidae</h2>
+            <img class="col-sm-6 col-md-4 item"><img class="img-fluid" src="assets/img/colletidaefamily.jpg" width="370px" height="300px"></img>
+            <h3>Subfamilias</h3>
+            <p>La familia de abejas <strong>Colletidae</strong> incluye las subfamilias:</p>
+                <li>Diphaglossinae</li>
+                <li>Euryglossinae</li>
+                <li>Hylaeinae</li>
+                <li>Xeromelissinae</li>
+            
+            <p></p>
+            <h3>Características</h3>
+            <p>Algunas de las características principales de este tipo de abejas son:</p>
+                <li>Las abejas de este grupo son solitarias.</li>
+                <li>Construyen los panales en el suelo o los árboles.</li>
+                <li>Las especies de esta familia habitan en todo el hemisferio sur, incluyendo Australia.</li>
+                <li>En algunas colonias conviven más de dos hembras que cumplen las mismas funciones.</li>
+                <li>Recubren el interior de los panales con una secreción proveniente de la glándula de Dufour, ubicada en el abdomen. Esta secreción se torna transparente e impermeable al contacto con el oxígeno.</li>
+
+            <p></p>
+            <h3>Especies</h3>
+            <p>Algunas especies de esta familia son:</p>
+                <li>Colletes albohirtus</li>
+                <li>Colletes albomaculatu</li>
+                <li>Hylaeus adriaticus</li>
+                <li>Cadegualina andina</li>
+                <li>Cadegualina sericata</li>
+            
+            <h2 class="text-center">Familia Halictidae</h2>
+            <img class="col-sm-6 col-md-4 item"><img class="img-fluid" src="assets/img/halictidaefamily.jpg" width="370px" height="300px"></img>
+            <h3>Subfamilias</h3>
+            <p>Otros de los tipos de abejas son los pertenecientes a la familia <strong>Halictidae</strong>, la cual incluye las siguientes subfamilias:</p>
+                <li>Halictinae</li>
+                <li>Nomiinae</li>
+                <li>Nomioidinae</li>
+                <li>Rophitinae</li>
+            
+            <p></p>
+            <h3>Características</h3>
+            <p>Estas abejas presentan organizaciones sociales diversas, así como otras características curiosas:</p>
+                <li>Algunas son solitarias.</li>
+                <li>Otras son parasociales, es decir, las colonias incluyen abejas de una sola generación.</li>
+                <li>Otras son eurosociales a un nivel primitivo, es decir, la diferencia de tamaño entre reina y obrera no es notoria, hay división de trabajo.</li>
+                <li>Lamen su transpiración durante el verano, razón por la cual son llamadas “abejitas del sudor”.</li>
+                <li>Anidan en el suelo y los árboles.</li>
+                <li>El cuerpo presenta coloraciones variadas: amarillo, negro, azul y verde.</li>
+
+            <p></p>
+            <h3>Especies</h3>
+            <p>Entre los tipos de abejas que pertenecen a esta familia se encuentran:</p>
+                <li>Ceylalictus celebensis</li>
+                <li>Ceylalictus cereus</li>
+                <li>Morawitzella nana</li>
+                <li>Ceratalictus ischnotes</li>
+                <li>Ceratalictus psoraspis</li>
+
+            <h2 class="text-center">Familia Megachildae</h2>
+            <img class="col-sm-6 col-md-4 item"><img class="img-fluid" src="assets/img/megachildaefamily.jpg" width="370px" height="300px"></img>
+            <h3>Subfamilias</h3>
+            <p>La familia Megachilidae incluye dos subfamilias:</p>
+                <li>Fideliinae</li>
+                <li>Megachilinae</li>
+            
+            <p></p>
+            <h3>Características</h3>
+            <p>Entre las características principales de este tipo de abejas, destaca:</p>
+                <li>Suelen ser solitarias.</li>
+                <li>Algunas especies poseen el hábito de que dos reinas compartan el mismo panal, aunque cada una se encarga de todo lo necesario para acondicionar su celda.</li>
+                <li>Algunas especies parasitan las colonias.</li>
+                <li>Construyen sus colonias con hojas secas y resinas que extraen de las plantas.</li>
+
+            <p></p>
+            <h3>Especies</h3>
+            <p>Algunas abejas que forman parte de esta familia son:</p>
+                <li>Fidelia hessei</li>
+                <li>Fidelia kobrowi</li>
+                <li>Pararhophites orobinus</li>
+                <li>Pararhophites quadratus</li>
+                <li>Radoszkowskiana gusevi</li>
+            
+            <h2 class="text-center">Familia Melittidae</h2>
+            <img class="col-sm-6 col-md-4 item"><img class="img-fluid" src="assets/img/melittidaefamily.jpg" width="370px" height="300px"></img>
+            <h3>Subfamilias</h3>
+            <p>La familia Melittidae incluye las subfamilias:</p>
+                <li>Dasypodainae</li>
+                <li>Meganomiinae</li>
+                <li>Melittinae</li>
+            
+            <p></p>
+            <h3>Características</h3>
+            <p>Las características principales de este tipo de abejas son:</p>
+                <li>Anidan en el suelo, donde abren agujeros en la tierra.</li>
+                <li>La mayoría de las especies de abejas Melittidae se encuentran en África, Europa y Asia, además de algunas zonas de Norteamérica. Por el contrario, no existen especies en la zona sur de América ni en Australia.</li>
+                <li>Son oligolécticas, es decir, solo recolectan el polen de un número determinado de especies vegetales.</li>
+
+            <p></p>
+            <h3>Especies</h3>
+            <p>Algunas abejas que forman parte de esta familia son:</p>
+                <li>Meganomia andersoni</li>
+                <li>Meganomia binghami</li>
+                <li>Dasypoda albipila</li>
+                <li>Dasypoda argentata</li>
+                <li>Afrodasypoda plumipes</li>
+            
+            <h2 class="text-center">Familia Stenotritidae</h2>
+            <img class="col-sm-6 col-md-4 item"><img class="img-fluid" src="assets/img/stenotritidaefamily.jpg" width="370px" height="300px"></img>
+            <h3>Subfamilias</h3>
+            <p>La familia <strong>Stenotritidae</strong> es el menor de todos los grupos de abejas e incluye los siguientes géneros:</p>
+                <li>Ctenocolletes</li>
+                <li>Stenotritus</li>
+            
+            <p></p>
+            <h3>Características</h3>
+            <p>Las características más llamativas de estas abejas son:</p>
+                <li>Las abejas de esta familia se encuentran en Australia.</li>
+                <li>Se distinguen por presentar cuerpos robustos cubiertos de vellosidades.</li>
+                <li>Anidan en el suelo.</li>
+                <li>Se diferencian por un vuelo veloz.</li>
+                <li>Al igual que las abejas de la familia Colletidae, recubren las celdas de los panales con una secreción impermeable.</li>
+
+            <p></p>
+            <h3>Especies</h3>
+            <p>Algunos de los tipos de abejas que pertenecen a esta familia son:</p>
+                <li>Ctenocolletes albomarginatus</li>
+                <li>Ctenocolletes centralis</li>
+                <li>Ctenocolletes fulvescens</li>
+                <li>Stenotritus elegans</li>
+                <li>Stenotritus elegantior</li>
+        </div>
     </div>
     <footer>
         <?php include 'footer.php'; ?>
