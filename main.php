1350cf75a7df02cca3042931de11c71e7ae9619f
Daniel Corona García <daniel.corona193@tectijuana.edu.mx>
2021-05-22 20:29:51 -0700

    Modificar datos

--- main.php
+++ main.php
@@ -4,10 +4,10 @@ require_once 'modelo2.php';
 
 $modelo = new Validacion();
 
-$modelo->NomUsr=$_POST['txtusrNomUsr'];
-$modelo->Contrasena=$_POST['txtusrContrasena'];
+$modelo->NomUsr = $_POST['txtusrNomUsr'];
+$modelo->Contrasena = $_POST['txtusrContrasena'];
 
-$filaLogear=$modelo->ValidacionUsr();
+$filaLogear = $modelo->ValidacionUsr();
 
 ?>
 
@@ -18,73 +18,100 @@ define('TITLE', 'Inicio');
 ?>
 
 <head>
-    <title>About - <?php echo TITLE ?></title>
-    <?php include 'head.php'; ?>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>About - <?php echo TITLE ?></title>
+  <?php include 'head.php'; ?>
+  <style type="text/css">
+    @import url("assets/bootstrap/css2/mycss.css");
+  </style>
+  <link href="assets/bootstrap/css2/bootstrap.css" rel="stylesheet" type="text/css">
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
 </head>
 
 <body>
 
-    <div class="container">
+  <div class="container">
 
-        <?php
-    if($filaLogear > 0){
-    $sql = "SELECT * FROM Usuario WHERE usrNomUsr='$modelo->NomUsr'";
-    $stmt = sqlsrv_query($conn,$sql);
-    if($stmt === false){
-        die(print_r(sqlsrv_errors(),true));
+    <?php
+    if ($filaLogear > 0) {
+      $sql = "SELECT * FROM Usuario WHERE usrNomUsr='$modelo->NomUsr'";
+      $stmt = sqlsrv_query($conn, $sql);
+      if ($stmt === false) {
+        die(print_r(sqlsrv_errors(), true));
         printf("Algo salio mal");
-    }
-    while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)){
-        echo "Bienvenido "; echo $row['usrNombre'];
-    }
-}
-else{
-    echo'<script type="text/javascript">
+      }
+      while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+        echo "Bienvenido ";
+        echo $row['usrNombre'];
+      }
+    } else {
+      echo '<script type="text/javascript">
     alert("El usuario o contraseña son incorrectos.");
     window.location.href="sesion.php";
     </script>';
-    exit();
-}
-?>
-<div id="contenido">
-      <table style="margin: auto; width: 800px; border-collapse: separate; border-spacing: 10px 5px;">
-        <thead>
-          <th>Id de localización.</th>
-          <th>Nombre de localización</th>
-          <th>Longitud</th>
-          <th>Latitud</th>
-          <th> <a href="nuevo_prod1.php"> <button type="button" class="btn btn-info">Nuevo</button> </a> </th>
-        </thead>
+      exit();
+    }
+    ?>
 
-        <?php
-        include "conexion.php";
-        $sql = "SELECT * FROM Localizacion";
-        $stmt = sqlsrv_query($conn, $sql);
-        if ($stmt === false) {
-          die(print_r(sqlsrv_errors(), true));
-          printf("Algo fallo");
-        }
-        while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
-          echo "<tr>";
-          echo "<td>"; echo $row['locId']; echo "</td>";
-          echo "<td>"; echo $row['locName']; echo "</td>";
-          echo "<td>"; echo $row['locLon']; echo "</td>";
-          echo "<td>"; echo $row['locLat']; echo "</td>";
-          echo "<td><a href='modif_loc.php?no=".$row['locId']."'> <button type='button' class='btn btn-success'>Modificar</button> </a></td>";
-          echo " <td><a href='eliminar_loc.php?no=".$row['locId']."'> <button type='button' class='btn btn-danger'>Eliminar</button> </a></td>";
-        echo "</tr>";
-        }
-        ?>
-      </table>
+    <div class="todo">
+
+      <div id="contenido">
+        <table style="margin: auto; width: 800px; border-collapse: separate; border-spacing: 10px 5px;">
+          <thead>
+            <th>Id de la Localización.</th>
+            <th>Nombre de la Localización</th>
+            <th>Descripción</th>
+            <th></th>
+            <th> <a href="nuevo_prod.php"> <button type="button" class="btn btn-info">Nuevo</button> </a> </th>
+          </thead>
+
+          <?php
+          include "conexion.php";
+          $sql = "SELECT * FROM Localizacion";
+          $stmt = sqlsrv_query($conn, $sql);
+          if ($stmt === false) {
+            die(print_r(sqlsrv_errors(), true));
+            printf("Algo fallo");
+          }
+          while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+            echo "<tr>";
+            echo "<td>";
+            echo $row['locId'];
+            echo "</td>";
+            echo "<td>";
+            echo $row['locName'];
+            echo "</td>";
+            echo "<td>";
+            echo $row['locLon'];
+            echo "</td>";
+            echo "<td>";
+            echo $row['locLat'];
+            echo "</td>";
+            echo "<td><a href='modif_loc.php?no=" . $row['locId'] . "'> <button type='button' class='btn btn-success'>Modificar</button> </a></td>";
+            echo " <td><a href='eliminar_prod.php?no=" . $row['locId'] . "'> <button type='button' class='btn btn-danger'>Eliminar</button> </a></td>";
+            echo "</tr>";
+          }
+          ?>
+        </table>
+      </div>
     </div>
-        
-    </div>
-    <footer>
-        <?php include 'footer.php'; ?>
-    </footer>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.js"></script>
-    <script src="assets/js/script.min.js"></script>
+
+
+
+
+  </div>
+  <footer>
+    <?php include 'footer.php'; ?>
+  </footer>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.js"></script>
+  <script src="assets/js/script.min.js"></script>
 </body>
 
 </html>
--- modif_loc.php
+++ modif_loc.php
@@ -2,23 +2,24 @@
 
 $consulta = ConsultarProducto($_GET['no']);
 
-function ConsultarProducto($id)
+function ConsultarProducto($no_prod)
 {
   include "conexion.php";
   $sql = "SELECT * FROM Localizacion WHERE locId= ?;";
-  $params = array($id);
+  $params = array($no_prod);
 
   $stmt = sqlsrv_query($conn, $sql, $params);
   if ($stmt === false) {
     die(print_r(sqlsrv_errors(), true));
     printf("Algo fallo");
-  } else
-    return [
-      $stmt['locId'],
-      $stmt['locName'],
-      $stmt['locLon'],
-      $stmt['locLat']
-    ];
+  }
+
+  return [
+    $stmt['locId'],
+    $stmt['locName'],
+    $stmt['locLon'],
+    $stmt['locLat']
+  ];
 }
 ?>
 <!DOCTYPE html>
@@ -28,7 +29,10 @@ function ConsultarProducto($id)
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Modificar Producto</title>
-  <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+  <style type="text/css">
+    @import url("assets/bootstrap/css2/mycss.css");
+  </style>
+  <link href="assets/bootstrap/css2/bootstrap.css" rel="stylesheet" type="text/css">
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
@@ -39,29 +43,34 @@ function ConsultarProducto($id)
 
 <body>
   <div class="todo">
-
     <div id="contenido">
       <div style="margin: auto; width: 800px; border-collapse: separate; border-spacing: 10px 5px;">
         <span>
           <h1>Modificar Producto</h1>
         </span>
         <br>
-        <form action="modif_loc2.php" method="POST">
-          <input type="hidden" name="habId">
+        <form action="modif_loc2.php" method="POST" style="border-collapse: separate; border-spacing: 10px 5px;">
+          <input type="hidden" name="locId" value="<?php echo $_GET['no'] ?>">
 
-          <label>Nombre de la habitación: </label>
-          <input type="text" id="Nombre" name="Nombre"><br>
-          <label>Longitud: </label>
-          <input type="text" id="Longitud" name="Longitud"><br>
-          <label>Latitud: </label>
-          <input type="text" id="Latitud" name="Latitud"><br>
+          <label>Nombre de la Localización: </label>
+          <input type="text" id="locName" name="locName" value="<?php echo $_GET[1] ?>"><br>
+
+          <label>Longitud de la Localización: </label>
+          <input type="text" id="locLon" name="locLon" value="<?php echo $_GET[1] ?>"><br>
+
+          <label>Latitud de la Localización: </label>
+          <input type="text" id="locLat" name="locLat" value="<?php echo $_GET[1] ?>"><br>
 
           <br>
           <button type="submit" class="btn btn-success">Guardar</button>
         </form>
       </div>
+
     </div>
+
   </div>
+
+
 </body>
 
 </html>
