c46f15682a01b5c60029326f051fd169ece1eff0
chrisare1718 <78322432+chrisare1718@users.noreply.github.com>
2021-05-22 22:10:34 -0700

    Implementacion de plantas

--- beeplants.php
+++ beeplants.php
@@ -17,7 +17,7 @@ define('TITLE', '¿Abejas para plantas?');
             <div class="row">
                 <div class="col-md-10 col-lg-8 mx-auto position-relative">
                     <div class="site-heading">
-                        <h1>¿Qué hay de nuevo?</h1><span class="subheading">Aquí encontraras cada una de nuestras nuevas investigaciones o trabajos.</span>
+                        <h1>Plantas</h1><span class="subheading">Las mejores aliadas para las abejas.</span>
                     </div>
                 </div>
             </div>
@@ -27,23 +27,116 @@ define('TITLE', '¿Abejas para plantas?');
         <section class="article-list">
             <div class="container">
                 <div class="intro">
-                    <h2 class="text-center">Bee Care</h2>
-                    <p class="text-center">Cambiemos la perspectiva en que se visualizan las abejas en México</p>
+                    <h2 class="text-center">Tipos de plantas favorables para las abejas</h2>
                 </div>
-                <div class="row articles">
-                    <div class="col-sm-6 col-md-4 item"><a href="#"><img class="img-fluid" src="assets/img/desk.jpg"></a>
-                        <h3 class="name">Article Title</h3>
-                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.</p><a class="action" href="#"><i class="fa fa-arrow-circle-right"></i></a>
-                    </div>
-                    <div class="col-sm-6 col-md-4 item"><a href="#"><img class="img-fluid" src="assets/img/building.jpg"></a>
-                        <h3 class="name">Article Title</h3>
-                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.</p><a class="action" href="#"><i class="fa fa-arrow-circle-right"></i></a>
-                    </div>
-                    <div class="col-sm-6 col-md-4 item"><a href="#"><img class="img-fluid" src="assets/img/loft.jpg"></a>
-                        <h3 class="name">Article Title</h3>
-                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.</p><a class="action" href="#"><i class="fa fa-arrow-circle-right"></i></a>
-                    </div>
-                </div>
+                <p>Las abejas realizan la polinización ya que a partir del polen de las plantas obtienen su principal fuente de proteínas y como alimento para sus crías.
+                    También se debe de considerar que las abejas utilizan las plantas con la finalidad de obtener el nectar de las flores y no solo para obtener polen.
+                </p>
+
+
+                <h1 class="text-center">La relación plantas-abejas</h1>
+                <img class="img-fluid" src="assets/img/relacionabejaplanta.jpg" width="300px" height="300px">
+                <p>
+                    Las plantas cuentan con células reproductivas a las cuales se les conoce como polen, donde al momento de realizar la polinización las abejas transportan dicho polen de una flor a otra
+                    lo cual permite que las plantas que florecen puedan producir semilla o fruta facilitando su reproducción.
+                    La abeja más conocida por realizar dicho proceso es la abeja europea de la miel <i>(Apis mellifera)</i> la cual se encarga de polinizar gran cantidad de cultivos
+                    debido a que no es muy selectivo o exigente al momento de seleccionar una planta a la cual polinizar.
+                </p> <br>
+
+                
+                <h1 class="text-center">¿Cómo realiza la polinización las abejas?</h1>
+                <img class="img-fluid" src="assets/img/abejapolinizando.jpg" width="500px" height="700px">
+                <p>
+                Cuando pensamos en una abeja, una de las cosas más llamativas es que sus cuerpos tienen “pelos”, los cuales en realidad se les conoce como setas, las cuales son diferentes según la parte del cuerpo de la abeja.
+
+                En el caso de las especies de abejas más selectivas, las setas presentan formas especializadas que facilitan la recolección y manipulación del polen. Las setas con formas muy especializadas se pueden ubicar en lugares muy específicos del cuerpo de la abeja.
+
+                Por otro lado, abejas que recolectan polen de las plantas tropicales tienen setas engrosadas alrededor de la boca. Las abejas que se especializan en plantas con granos de polen muy grandes, poseen setas muy separadas entre sí, lo cual permite que los granos de polen se incrusten y sean transportados por la abeja.
+                </p> <br>
+
+                <p>
+                <h1 class="text-center">Tipos de polinización</h1>
+                Incidental: el objetivo principal de forrajeo es el néctar (también aceites o resinas) y el polen se acumula en el cuerpo de la abeja de manera pasiva, ya sea por la consistencia pegajosa del polen, sus ornamentaciones o por electrostática, mientras la abeja se mueve por la flor en busca de néctar. El polen se pega de manera indistinta en el cuerpo de la abeja y puede descartarlo cuando se limpia o lo puede trasladar a las áreas especializadas del cuerpo (escopa o corbícula) para ser transportado al nido.
+
+                Activa: el objetivo principal del forrajeo es el polen. Dentro de esta categoría se han descrito seis formas en las que las abejas obtienen el polen:<br>
+                <ul>
+                    <li>Raspar con las extremidades: las abejas usan las patas o las partes bucales para raspar el polen directamente de las anteras.</li><br>
+                    <li>Vibrar: las abejas usan contracciones de los músculos del tórax para producir vibraciones que son transmitidas del cuerpo de la abeja a las anteras, de manera que el polen es expulsado, este comportamiento también es conocido como polinización por vibración y se da en flores donde el polen está guardado dentro de las anteras.</li><br>
+                    <li>Frotar con el cuerpo o las escopas: las abejas frotan continuamente las anteras con su tórax y abdomen o con la zona de las patas donde se recolecta el polen (escopa), el movimiento realizado es adelante-atrás, arriba-abajo sobre la flor. </li><br>
+                    <li>Golpeteo: las abejas recolectan el polen usando golpecitos rápidos con el abdomen, el movimiento es arriba-abajo.</li><br>
+                    <li>Frotar con la cara: las abejas frotan las anteras continuamente con la cara. Donde las abejas usan los “pelos” para recolectar el polen. </li><br>
+                    <li>Raspar: este comportamiento solo ha sido observado en abejas que recolectan polen de flores del género Penstemon (familia Plantaginaceae; Campanitas); las anteras están parcialmente guardadas dentro de la corola de la flor. Para ello, las abejas frotan las anteras con la parte dorsal del tórax, moviendo todo el cuerpo dentro y fuera de la flor, esta fricción genera vibraciones que liberan el polen de las anteras.</li><br>
+                </ul>
+                </p>
+
+                <h1 class="text-center">Plantas preferidas de las abejas</h1>
+                <bro><br>
+
+                    <h3>Lavanda</h3>
+                    <img src="assets/img/lavanda.jpg" width="400px" height="400px">
+                    <p>
+                        A pesar de que no hay registros en el SIAP, se ha identificado que en Puebla es la entidad pionera en su cultivo y transformación, por lo que se espera que la superficie producida se intensifique en los próximos años. Además de que se ve hermosa por sus flores moradas y por su elegancia melancólica, atrae a muchas abejas, mariposas, escarabajos y polinizadores en general.
+                    </p><br>
+
+                    <h3>Orégano </h3>
+                    <img src="assets/img/oregano.jpg" width="400px" height="400px">
+                    <p>
+                        México es considerado uno de los principales productores y comercializadores de orégano donde se tiene a ciertas entidades como principales productoras del país, entre ellas están: Baja California Sur, Coahuila, Chihuahua, Campeche, Durango, Guerrero, Hidalgo, Jalisco, Nuevo León, Oaxaca, Puebla, Querétaro, San Luis Potosí, Sonora, Sinaloa, Tamaulipas, Veracruz, Yucatán, Zacatecas y Guanajuato.
+
+                        Este tipo de planta atrae a las abejas polinizadoras por su aroma y por sus pequeñas flores de color blanco y rojo que llaman la atención de estas.
+                    </p><br>
+
+                    <h3>Caléndula </h3>
+                    <img src="assets/img/calendula.jpg" width="400px" height="400px">
+                    <p>
+                        Este tipo de flor es ideal para las abejas ya que si bien este tipo de flor por su color anaranjado atrae alas abejas, también este tipo de flor crecen todo el año y florece en todas las estaciones. Los principales estados donde se puede encontrar son: Baja California, Chihuahua, Sinaloa, Durango, Jalisco, Nuevo León, Guanajuato, Veracruz, San Luis Potosí, Oaxaca, Puebla, Hidalgo y Estado de México.
+                    </p><br>
+
+                    <h3>Romero </h3>
+                    <img src="assets/img/romero.jpg" width="400px" height="400px">
+                    <p>
+                        Esta planata cuenta con la peculiaridad que solo se produce en 2 estados que son: Estado de México y Baja California.
+                    </p><br>
+
+                    <h3>Algodoncillo </h3>
+                    <img src="assets/img/algodoncillo.jpg" width="400px" height="400px">
+                    <p>
+                        Esta planta atrae a las abejas por sus llamativos colores naranja y amarillo, usualmente se encuentran en: Baja California Sur, Sinaloa, Sonora, Nuevo León y el resto del sur del país.
+                    </p><br>
+
+                    <h3>Tejocote </h3>
+                    <img src="assets/img/tejocote.jpg" width="400px" height="400px">
+                    <p>
+                        Este tipo de flor usualmente florece en enero y abril y las abejas se sienten atraidas a estas por su peculiar aroma. Esta planta se encuentra principalmente en: Ciudad de México, Estado de México, Sinaloa, Hidalgo, Chiapas, Oaxaca, Guanajuato, Michoacan, Jalisco y San Luis Potosí.
+                    </p><br>
+
+                    <h3>Muicle </h3>
+                    <img src="assets/img/muicle.jpg" width="400px" height="400px">
+                    <p>
+                        El muicle es una pequeña flor de color naranja en forma de tubo, donde sus hojas son venenosas. Principalmente esta flor se localiza en: Baja California Sur, Sinaloa, Nuevo León, Jalisco, Puebla, Estado de México, Quintana Roo, Chiapas, Tamaulipas, Hidalgo y Tabasco.
+                    </p><br>
+
+                    <h3>Mirto Morado </h3>
+                    <img src="assets/img/mirtomorado.jpg" width="400px" height="400px">
+                    <p>
+                        Es una planta herbácea con inflorescencias de color violeta-azul con las hojas estrechas y sin embargo, brillantes que la diferencia de otras salvias que llevan hojas aterciopeladas y opacas. Esta planta requiere de sol total o parcial y crecerá hasta los 90 cm o más con buena tierra. Atrae a las abejas, colibríes y mariposas.
+
+                        Esta planta se encuentra en: Nuevo León, Chihuahua y Coahuila.
+                    </p><br>
+
+                    <h3>Cordoncillo</h3>
+                    <img src="assets/img/cordoncillo.jpg" width="400px" height="400px">
+                    <p>
+                        El Cordón de Jesús es un arbusto herbáceo que mide hasta 1.20 metros de altura. Las hojas son largas, de hasta 15 cm por 18 mm y de un color verde grisáceo, de textura aterciopelada y muy aromáticas.
+
+                        Estas bellas flores son verdaderos atractivos de abejas, mariposas y aves como los colibríes. Este tipo de planta se puede encontrar en: Nuevo León, Guanajuato, Chiapas, Puebla, Querétaro y Ciudad de México.
+                    </p><br>
+
+                    <h3>Tronadora </h3>
+                    <img src="assets/img/tronadora.jpg" width="400px" height="400px">
+                    <p>
+                        Árbol chico de 1 a 10m de altura compuesta de hojas amarillas en forma de lámpara, este color es el que atrae a las abejas, usualmente se encuentra en los estados de: Baja California Sur, Nuevo León, Sinaloa, Chihuahua, Durango, Oaxaca,Yucatán, Chiapas, Hidalgo y Guanajuato.
+                    </p><br>
             </div>
         </section>
     </div>
